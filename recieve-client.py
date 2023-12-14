5c2e77c7f4b2240f09d4be036f17b86aff626dd0
Захар Братчиков <zaharbratcikov09817@gmail.com>
2023-12-14 12:47:49 +0500

    '

---
+++ recieve-client.py
@@ -0,0 +1,27 @@
+import socket
+import sys
+
+def start_receive_client(local_port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind(('0.0.0.0', local_port))  # Привязка к любому доступному интерфейсу
+
+    try:
+        print(f"Сервер получения запущен на порту {local_port}")
+
+        while True:
+            # Получение данных
+            data, addr = server_socket.recvfrom(1024)
+            print(f"Получено от {addr}: {data.decode('utf-8')}")
+
+    except KeyboardInterrupt:
+        print("\nСервер получения остановлен.")
+    finally:
+        server_socket.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Использование: python receive_client.py <порт>")
+        sys.exit(1)
+
+    local_port = int(sys.argv[1])
+    start_receive_client(local_port)
---
+++ send-client.py
@@ -0,0 +1,28 @@
+import socket
+import sys
+
+def start_send_client(server_host, server_port):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    try:
+        while True:
+            message = input("Введите данные для отправки (или 'exit' для выхода): ")
+            if message.lower() == 'exit':
+                break
+
+            # Отправка данных на заданный адрес и порт
+            client_socket.sendto(message.encode('utf-8'), (server_host, server_port))
+
+    except KeyboardInterrupt:
+        print("\nКлиент отправки остановлен.")
+    finally:
+        client_socket.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Использование: python send_client.py <ip-адрес сервера> <порт сервера>")
+        sys.exit(1)
+
+    server_host = sys.argv[1]
+    server_port = int(sys.argv[2])
+    start_send_client(server_host, server_port)
